22e8773d515fc4d52ebc89e6372fcea37b214364
csaflip <scolinadams@gmail.com>
2024-10-03 22:48:54 -0600

    use activity service fetch

--- appcore/src/app/vert/vert.component.ts
+++ appcore/src/app/vert/vert.component.ts
@@ -1,5 +1,7 @@
 import { Component, Inject, OnInit } from "@angular/core";
 import { OPEN_RESOURCE_RESOLVER, OpenResourceResolver } from "../shared/services/links-opener/open-resource-resolver";
+import { ActivityService } from "../shared/services/activity/activity.service";
+import { DesktopActivityService } from "../shared/services/activity/impl/desktop-activity.service";
 
 @Component({
   selector: "app-donate",
@@ -13,11 +15,19 @@ export class VertComponent implements OnInit {
 
   public donateUrl: string;
 
-  constructor(@Inject(OPEN_RESOURCE_RESOLVER) private readonly openResourceResolver: OpenResourceResolver) {}
+  constructor(
+    @Inject(OPEN_RESOURCE_RESOLVER) private readonly openResourceResolver: OpenResourceResolver,
+    @Inject(ActivityService) protected readonly activityService: DesktopActivityService
+  ) {}
 
   public ngOnInit() {
     this.donateUrl =
       VertComponent.PAYPAL_ACCOUNT_BASE_URL + "/" + VertComponent.DEFAULT_AMOUNT + VertComponent.DEFAULT_CURRENCY;
+
+    const x = this.activityService.fetch().then(data => {
+      console.log(data);
+    });
+    console.log(x);
   }
 
   public onDonateClicked() {
